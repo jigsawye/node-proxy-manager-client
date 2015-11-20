ca8b1664c56d849d304d0e8b9627b45ec1ff7a44
jigsawye <jigsaw.ye@gmail.com>
2015-11-20 23:19:37 +0800

    move token to header and fixed bug of GET with body

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,16 +2,21 @@ import fetch from 'isomorphic-fetch';
 import { getToken } from './auth';
 
 export default function fetchAPI(method, path, data = {}) {
-  return fetch(`http://${window.location.hostname}:3000/${path}`, {
+  const options = {
     method: method,
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(Object.assign(data, {
-      token: getToken() || '',
-    })),
-  }).then(res =>
+      'Content-Type': 'application/json',
+      'authorization': `Bearer ${getToken() || ''}`
+    }
+  };
+
+  if (method !== 'get') {
+    options.body = JSON.stringify(data);
+  }
+
+  return fetch(`http://${window.location.hostname}:3000/${path}`, options)
+  .then(res =>
     res.json().then(json => ({ json, res }))
   ).then(({ json, res }) => {
     if (! res.ok) {
