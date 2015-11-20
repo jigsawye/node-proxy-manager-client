054062a3b5189386593dd4b52e3ecd3552f2dea2
jigsawye <jigsaw.ye@gmail.com>
2015-11-20 16:33:08 +0800

    add proxies actions and reducers

--- src/actions/proxy.js
+++ src/actions/proxy.js
@@ -1,3 +1,36 @@
+import fetchAPI from '../utils/fetch';
+
+export const APP_LOADING = 'APP_LOADING';
+
+function appLoading() {
+  return {
+    type: APP_LOADING,
+  };
+}
+
 export const PROXIES_REQUEST = 'PROXIES_REQUEST';
 export const PROXIES_SUCCESS = 'PROXIES_SUCCESS';
 export const PROXIES_FAILURE = 'PROXIES_FAILURE';
+
+function proxiesSuccess(proxies) {
+  return {
+    type: PROXIES_SUCCESS,
+    proxies,
+  };
+}
+
+function proxiesFailure(err) {
+  return {
+    type: PROXIES_FAILURE,
+    err,
+  };
+}
+
+export function proxiesRequest() {
+  return dispatch => {
+    dispatch(appLoading());
+    return fetchAPI('get', '/proxies')
+      .then(json => dispatch(proxiesSuccess(json)))
+      .catch(json => dispatch(proxiesFailure(json)));
+  };
+}
---
+++ src/reducers/proxy.js
@@ -0,0 +1,25 @@
+import {
+  APP_LOADING,
+  PROXIES_SUCCESS,
+  PROXIES_FAILURE,
+} from '../actions/proxy';
+
+const defaultState = {
+  proxies: [],
+  isError: false,
+  errorMessage: '',
+  isLoading: false,
+};
+
+export default function proxy(state = defaultState, action) {
+  switch (action.type) {
+  case APP_LOADING:
+    return Object.assign({}, state, { isLoading: true });
+  case PROXIES_SUCCESS:
+    return Object.assign({}, state, { proxies: action.proxies, isLoading: false });
+  case PROXIES_FAILURE:
+    return Object.assign({}, defaultState, { isError: true, errorMessage: action.err.error });
+  default:
+    return state;
+  }
+}
